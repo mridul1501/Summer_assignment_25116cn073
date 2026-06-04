bdc6dcc10463d9b2059165732ddf34394b0ea186
mridul1501 <jayantsharma1502@gmail.com>
2026-06-04 15:48:56 +0530

    day1

---
+++ Day1/p2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class p2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+
+        int n = input.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + "*" + i + "=" + (n * i));
+        }
+        input.close();
+    }
+}
