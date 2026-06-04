1e6627bd60cfc5ef273725d4f3a6bbacffa05a3c
mridul1501 <jayantsharma1502@gmail.com>
2026-06-04 16:16:04 +0530

    d

--- Day2/p5.java
+++ Day2/p5.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class P5 {
+public class p5 {
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
         System.out.print("Enter the number : ");
