93dad9a5750aa63fabba5d241fa1b0b33222450f
mridul1501 <jayantsharma1502@gmail.com>
2026-06-09 17:51:58 +0530

    d

---
+++ Day9/P36_HollowSqPattern.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class P36_HollowSqPattern {
+
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.print("Enter number of rows : ");
+        int n = num.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || j == 1 || i == n || j == n) {
+                    System.out.print("* ");
+                } else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+        num.close();
+    }
+
+}
