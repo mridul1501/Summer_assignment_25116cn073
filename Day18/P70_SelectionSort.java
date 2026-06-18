c8f72149351bf6256821bc5c9a1ac2b6a0199d7b
mridul1501 <jayantsharma1502@gmail.com>
2026-06-18 18:58:40 +0530

    d18

--- Day18/P70_SelectionSort.java
+++ Day18/P70_SelectionSort.java
@@ -1,49 +1,53 @@
 
 import java.util.Scanner;
 
+public class P70_SelectionSort {
 
-public class P69_BubbleSort {
+   
+    static void SelectionSort(int a[], int n) {
+        int temp, min;
+        for (int i = 0; i < n - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[min])
+                    min = j;
+            }
 
-    
-    static void BubbleSort(int a[], int n) {
-        int temp;
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < n - i; j++) {
-                if (a[j] > a[j + 1]) {
-                    temp = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = temp;
-                }
+            if (min != i) {
+                temp = a[i];
+                a[i] = a[min];
+                a[min] = temp;
             }
         }
 
     }
 
+  
     static void print(int a[], int n) {
         for (int i = 0; i < n; i++) {
             System.out.print(a[i] + "\s\s\s");
         }
     }
 
-    
+
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
         System.out.print("Enter the size of an Array : ");
         int n = num.nextInt();
         int[] a = new int[n];
-        
+     
         for (int i = 0; i < n; i++) {
             System.out.print("a[" + i + "] = ");
 
             a[i] = num.nextInt();
         }
-        
+      
         System.out.println("UNSORTED ARRAY : ");
         print(a, n);
         System.out.println();
-        // FUNCTION CALL FOR SORTING
-        BubbleSort(a, n);
-
+        
+        SelectionSort(a, n);
+   
         System.out.println("SORTED ARRAY : ");
         print(a, n);
 
