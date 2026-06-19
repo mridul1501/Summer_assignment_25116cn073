ac52263f945de72386202c8868b0a0289495f59c
mridul1501 <jayantsharma1502@gmail.com>
2026-06-19 18:40:54 +0530

    d19

---
+++ Day19/P74_matrixSubtract.java
@@ -0,0 +1,54 @@
+
+import java.util.Scanner;
+
+
+public class P74_matrixSubtract {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter row of first matrix: ");
+        int r1 = input.nextInt();
+        System.out.print("Enter column of first matrix: ");
+        int c1 = input.nextInt();
+        System.out.print("Enter row of first matrix: ");
+        int r2 = input.nextInt();
+        System.out.print("Enter column of first matrix: ");
+        int c2 = input.nextInt();
+        int[][] a = new int[r1][c1];
+        int[][] b = new int[r2][c2];
+        int[][] sub = new int[r1][c1];
+
+        if (r1 == r2 && c1 == c2) {
+            for (int i = 0; i < r1; i++) {
+                for (int j = 0; j < c1; j++) {
+                    System.out.print("Enter a[" + i + "," + j + "]: ");
+                    a[i][j] = input.nextInt();
+                }
+            }
+            for (int i = 0; i < r2; i++) {
+                for (int j = 0; j < c2; j++) {
+                    System.out.print("Enter b[" + i + "," + j + "]: ");
+                    b[i][j] = input.nextInt();
+                }
+            }
+            for (int i = 0; i < r1; i++) {
+                for (int j = 0; j < c1; j++) {
+                    sub[i][j] = a[i][j] - b[i][j];
+                }
+            }
+            for (int i = 0; i < r1; i++) {
+                for (int j = 0; j < c1; j++) {
+                    System.out.print(sub[i][j] + "\s\s\s");
+
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.print("Subtraction is not possible");
+        }
+
+        input.close();
+    }
+
+}
